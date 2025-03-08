45cf1922ab6d13d511f07145be63cf35b6e264be
Jihad Chaalan <chaalan2004@gmail.com>
2025-03-08 17:10:27 +0200

    Update the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-// import TestAPI from "./test";
+import TestAPI from "./test";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      {/* <TestAPI /> */}
+      <TestAPI />
     </>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,12 @@ import "../Home/Home.css";
 const Home = () => {
   const [movies, setMovies] = useState([]);
 
+  const getMovies = async () => {
+    const movieData = await fetchTrendingMovies();
+    setMovies(movieData);
+  };
+
   useEffect(() => {
-    const getMovies = async () => {
-      const movieData = await fetchTrendingMovies();
-      setMovies(movieData);
-    };
     getMovies();
   }, []);
 
@@ -28,16 +29,25 @@ const Home = () => {
           Millions of movies, TV shows and people to discover.
         </p>
       </div>
-      <h1>Trending Movies</h1>
-      {movies.length > 0 ? (
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div className="Trending-movies">
+        <h1 className="TM-header">Trending Movies</h1>
+        {movies.length > 0 ? (
+          <ul className="movies-list">
+            {movies.map((movie) => (
+              <li key={movie.id} className="movie-item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                  style={{ width: "200px", borderRadius: "10px" }}
+                />
+                <p className="movie-title">{movie.title}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };

2ac01a30cf55831d8f74dc9b80606a3216538c06
Jihad Chaalan <chaalan2004@gmail.com>
2025-03-13 17:37:24 +0200

    Update Trending movies in Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,18 +2,36 @@
 import { useEffect, useState } from "react";
 import { fetchTrendingMovies } from "../../util/API.js";
 import "../Home/Home.css";
-
+import videoIconRight from "../../assets/iconsRight.svg";
+import videoIconLeft from "../../assets/iconsLeft.svg";
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const getMovies = async () => {
     const movieData = await fetchTrendingMovies();
     setMovies(movieData);
   };
 
+  const displayedMovies = movies.slice(currentIndex, currentIndex + 4);
+
+  console.log(currentIndex);
+
   useEffect(() => {
     getMovies();
   }, []);
+
+  const handleNext = () => {
+    if (currentIndex + 4 < movies.length) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex - 1 >= 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
 
   return (
     <div className="Home-container">
@@ -32,18 +50,26 @@ const Home = () => {
       <div className="Trending-movies">
         <h1 className="TM-header">Trending Movies</h1>
         {movies.length > 0 ? (
-          <ul className="movies-list">
-            {movies.map((movie) => (
-              <li key={movie.id} className="movie-item">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt={movie.title}
-                  style={{ width: "200px", borderRadius: "10px" }}
-                />
-                <p className="movie-title">{movie.title}</p>
-              </li>
-            ))}
-          </ul>
+          <div className="movies-container">
+            <button className="scroll-button prev" onClick={handlePrev}>
+              <img src={videoIconLeft} alt="cinema-" />
+            </button>
+            <ul className="movies-list">
+              {displayedMovies.map((movie) => (
+                <li key={movie.id} className="movie-item">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    style={{ width: "200px", borderRadius: "10px" }}
+                  />
+                  <p className="movie-title">{movie.title}</p>
+                </li>
+              ))}
+            </ul>
+            <button className="scroll-button next" onClick={handleNext}>
+              <img src={videoIconRight} alt="cinema-" />
+            </button>
+          </div>
         ) : (
           <p>Loading...</p>
         )}
